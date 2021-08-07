03f309c259a17f4cf5ac772c394dc6744b73d015
yuanweibo <2435722121@qq.com>
2021-08-07 23:40:52 +0800

    配置项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import Home from "./pages/Home/index";
+import CityList from "./pages/CityList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/">首页11</Link>
+          <Link to="/city-list">城市选择11</Link>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/city-list" component={CityList} />
+        </div>
+      </Router>
+    );
   }
 }
 
---
+++ src/pages/CityList/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CityList extends Component {
+  render() {
+    return <div>城市选择</div>;
+  }
+}
+
+export default CityList;
